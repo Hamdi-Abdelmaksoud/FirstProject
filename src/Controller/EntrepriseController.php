3e3f89c189001a42815695960283b3f3c28264a3
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2023-05-26 14:43:59 +0100

    Formulaire entreprise

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Entreprise;
+use App\Form\EntrepriseType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,13 +15,38 @@ class EntrepriseController extends AbstractController
     #[Route('/entreprise', name: 'app_entreprise')]
     public function index(ManagerRegistry $doctrine): Response
     {
-        $entrepries = $doctrine->getRepository(Entreprise::class)->findAll();
+        $entreprises = $doctrine->getRepository(Entreprise::class)->findAll();
         $arr = ["val1", "val2"];
         return $this->render('entreprise/index.html.twig', [
-            "entreprises" => $entrepries
+            "entreprises" => $entreprises
         ]);
     }
   
+    #[Route('/entreprise/add', name: 'add_entreprise')]  
+    /* ManagerRegistry pour intéragir avec la base de données entreprise type elemnt*/
+    public function add(ManagerRegistry $doctrine,Entreprise $entreprise=null,Request $request):Response
+    {
+        /*entrepriseType from form  entreprise, $entreprise en paramétres*/
+        $form=$this->createForm(EntrepriseType::class,$entreprise);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()&& $form->isValid())
+        {
+            $entreprie=$form->getData();
+            //getManager() pour accéeder au persiste et flush.
+            $entityManager=$doctrine->getManager();
+            //prepare
+            $entityManager->persist($entreprise);
+            //insert into (excute)
+            $entityManager->flush();
+        return $this->redirectToRoute('app_entreprise');
+
+        }
+        return $this->render('entreprise/add.html.twig',[
+          'formAddEntreprise'=>$form->createView()  
+        ]);
+
+    }
     #[Route('/entreprise/{id}', name: 'show_entreprise')]
     public function show(Entreprise $entreprise): Response
     {
@@ -27,6 +54,6 @@ class EntrepriseController extends AbstractController
         return $this->render('entreprise/show.html.twig', [
             'entreprise' => $entreprise
         ]);
-        
-    }
+      
+}
 }
